e6fc4c4dbbed7c08d9e315cdc4b29ee38895a13b
SilentStorm <1449488533qq@gmail.com>
2021-03-01 21:33:03 +0800

    Jdk11 新特性

--- src/main/java/demo/java/hello/Main.java
+++ src/main/java/demo/java/hello/Main.java
@@ -3,6 +3,9 @@ package demo.java.hello;
 import demo.kotlin.Hello;
 import demo.kotlin.Person;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author lihu
  */
@@ -12,5 +15,9 @@ public class Main {
         String message = hello.sayHello("Lihu");
         System.out.println(message);
         Person person = new Person("li");
+        //jdk10 添加的新特性，局部变量可以使用var 进行类型推导
+        var list = new ArrayList<String>();
+        var stream = list.stream();
+        var sringList = List.of("A", "B", "C");
     }
 }
